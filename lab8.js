a41d5fdc55a3b94f5d14ad53cdeba2290bb44e2f
kka <k.kurilowa2018@yandex.ru>
2023-05-31 11:34:27 +0700

    Количество дней с даты рождения

--- lab8.js
+++ lab8.js
@@ -11,3 +11,20 @@ function showDate() {
     let swe = document.getElementById('current-date5');
     swe.innerHTML = 'Дата для локали Swedish: ' + today.toLocaleString('sv');
 }
+
+function showDaysCount(){
+    let today = new Date();
+    let inputDate = document.querySelector('input[type=date]');
+    let birthday = new Date(inputDate.value);
+    let dayscount = (today - birthday)/1000/60/60/24;
+    dayscount = Math.floor(dayscount);
+    document.getElementById('dateb').innerHTML = 'Количество дней с даты рождения: ' + dayscount;
+}
+
+function udal() {
+    let today = new Date();
+    let inputDate = document.querySelector('input[type=date]');
+    inputDate.value = "";
+    dayscount = 0;
+    document.getElementById('dateb').innerHTML = 'Количество дней с даты рождения: ' + dayscount;
+}
